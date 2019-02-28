26412e05d58afe960256b97b9279decff16daa2e
cuongngo <cuongnm265@gmail.com>
2019-02-28 20:48:04 +0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  with_options controller: :questions do
+    get 'question1/answer', action: :question1
+    get 'question2/answer', action: :question2
+    get 'question3/answer', action: :question3
+    get 'question4/answer', action: :question4
+    get 'question5/answer', action: :question5
+    get 'question6/answer', action: :question6
+  end
 end
